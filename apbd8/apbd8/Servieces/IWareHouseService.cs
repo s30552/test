e7fa1571d00351ca0a3aa7e98ce140af973f1ec6
s30552 <s30552@pjwstk.edu.pl>
2025-05-09 18:29:37 +0200

    Delete apbd8 directory

--- apbd8/apbd8/Servieces/IWareHouseService.cs
+++
@@ -1,9 +0,0 @@
-using apbd8.Models;
-
-namespace apbd8.Servieces;
-
-public interface IWareHouseService
-{
-    Task<int> AddProductToWarehouseAsync(AddToWarehouseDto dto);
-    Task<int> AddProductToWarehouseViaProcAsync(AddToWarehouseDto dto);
-}
